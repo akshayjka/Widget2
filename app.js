4203fc433b2974ce6efcc182f030911f413b93c0
akshayjka <akshayjai19001900@gmail.com>
2024-01-11 12:38:56 +0530

    3st commit

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const corsOptions = {
   optionsSuccessStatus: 204,
 };
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(cors(corsOptions));
 
 app.use(logger('dev'));
