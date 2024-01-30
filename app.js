dc90e6a58592c6eef4959daa2890de9e2ed69705
akshayjka <akshayjai19001900@gmail.com>
2024-01-30 14:46:38 +0530

    3st commit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const corsOptions = {
 };
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://demoapp.sysgrate.com'); // or specify your domain
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
